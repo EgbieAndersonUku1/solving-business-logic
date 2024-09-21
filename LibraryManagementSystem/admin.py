23eafc39cf37e58ede9042e8d4c557d266550af0
EgbieAndersonUku1 <jayunderwood2011@hotmail.com>
2024-09-21 22:48:03 +0100

    feat(models): Add methods to the various models for library management system based on readme.md problem
    - Added more models for Book, Author, Member, and Borrowing to represent the core entities of the library management system.
    - Defined fields and relationships:
        - Number of books borrowed for a given user
        - Libraries associated with a given user
        - Integrated the `overdue_books` method from `BorrowBook` model into `LibraryAdmin`.
        - Display the number of overdue books in the library admin list view.
        - Enhanced the admin display methods for better readability and usability.
        - Available books
        - Reserved books
        - None available books
        - display a list overdue books for a given library
    - Relationships between models established, but full system functionality (e.g., borrowing logic) is not yet implemented.

--- LibraryManagementSystem/admin.py
+++ LibraryManagementSystem/admin.py
@@ -1,15 +1,38 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models import Q
+from django.db.models.query import QuerySet
+from django.http import HttpRequest 
 
 # Register your models here.
-from .models import Library, Member, Book, BorrowRecord, Author
+from .models import Library, Member, Book, BorrowBook, Author
 
 
 
 class LibraryAdmin(admin.ModelAdmin):
-    list_display       = ["id", "name"]
+    list_display       = ["id", "name", "book_count", "available_books", "non_available_books", "reserved_books", "overdue_books"]
     search_fields      = ["name"]
     list_display_links = ["id", "name"]
     
+    def book_count(self, obj):
+        return obj.book_count()
+    
+    def available_books(self, obj):
+        return obj.available_books()
+    
+    def non_available_books(self, obj):
+        return obj.non_available_books()
+    
+    def reserved_books(self, obj):
+        return obj.reserved_books()
+    
+    def overdue_books(self, obj):
+        """Get the number of overdue books the library has"""
+        return BorrowBook.overdue_books(obj)
+    
+    
+    book_count.short_description = "Total number of books"
+    reserved_books.short_description = "Num of reserved books"
 
 
 class MemberAdmin(admin.ModelAdmin):
@@ -20,33 +43,62 @@ class MemberAdmin(admin.ModelAdmin):
         return obj.total_libraries
     
     def total_num_of_books_borrowed(self, obj):
-        return object.total_num_of_books_borrowed
+        return obj.total_num_of_books_borrowed
 
 
 class AuthorAdmin(admin.ModelAdmin):
-    list_display    = ["first_name", "last_name", "biography"]
-    search_fields   = ["first_name", "last_name"]
-    readonly_fields = ["created_on", "modified_on"]
+    list_display       = ["id", "first_name", "last_name", "count_written_books", "created_on", "modified_on"]
+    list_display_links = ["id", "first_name"]
+    search_fields      = ["first_name", "last_name"]
+    readonly_fields    = ["created_on", "modified_on"]
     
+    def count_written_books(self, obj):
+      """Count the number of books written by the author"""
+      return obj.count_written_books()
 
 
 class BookAdmin(admin.ModelAdmin):
-    list_display    = ["id", "title", "ISBN", "publication_date", "genre", "created_on", "num_of_authors"]
-    search_fields   = ["title", "ISBN", "genre"]
-    readonly_fields = ["created_on", "modified_on"]
-    
+    list_display       = ["id", "title", "ISBN", "publication_date", "genre", "created_on", "num_of_authors", "library_location"]
+    list_display_links = ["title", "ISBN"]
+    search_fields      = ["title", "ISBN", "genre"]
+    readonly_fields    = ["created_on", "modified_on", "ISBN"]
     
     def num_of_authors(self, obj):
         """The number of authors for a given book"""
         return obj.num_of_authors
     
+    def library_location(self, obj):
+        """The location where the book can be found"""
+        return obj.library_location.title()
     
     
-class BorrowRecordAdmin(admin.ModelAdmin):
-    list_display = []
+    
+class BorrowBookAdmin(admin.ModelAdmin):
+    list_display = ["member__first_name", "member__last_name", "member__email", "book", "status", "due_date", "return_date", "is_overdue"]
+    search_fields   = ["library__name", "book__title", "member__first_name", "member__email"]
+    
+    def get_search_results(self, request: HttpRequest, queryset: QuerySet[Any], search_term: str) -> tuple[QuerySet[Any], bool]:
+        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
+        
+        if search_term:
+            queryset = queryset.filter(
+                Q(library__name__icontains=search_term) |
+                Q(book__title__icontains=search_term) |
+                Q(member__first_name__icontains=search_term) |
+                Q(member__last_name__icontains=search_term) |
+                Q(member__email__icontains=search_term)
+            )
+
+        return queryset, use_distinct
+
+    def is_overdue(self, obj):
+        return obj.is_overdue()
+    
+  
 
 admin.site.register(Library, LibraryAdmin)
 admin.site.register(Member, MemberAdmin)
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(Book, BookAdmin)
+admin.site.register(BorrowBook, BorrowBookAdmin)
     
--- LibraryManagementSystem/models.py
+++ LibraryManagementSystem/models.py
@@ -1,57 +1,129 @@
 from django.db import models
-
+from django.utils import timezone
 
 # Create your models here.
 
 class Library(models.Model):
-    name             = models.CharField(max_length=40, unique=True)
+    name = models.CharField(max_length=40, unique=True)
     max_borrow_limit = models.PositiveSmallIntegerField()
     
     class Meta:
-        verbose_name        = "Library"
+        verbose_name = "Library"
         verbose_name_plural = "Libraries"
         
     def __str__(self):
-        return f"Libray name - {self.name}"
+        return f"Library name - {self.name}"
     
-    def issue_book(self, book_name):
-        pass
+    def book_count(self):
+        """
+        Returns the total number of books associated with this library.
+
+        Returns:
+            int: The total number of books in the library.
+        """
+        return self.books.count()
+    
+    def available_books(self):
+        """
+        Returns the number of books available to be reserved or borrowed.
+
+        Returns:
+            int: The count of available books.
+        """
+        return self.books.filter(is_book_available=True).count()
+    
+    def non_available_books(self):
+        """
+        Returns the number of books that are not available to be borrowed.
+
+        Returns:
+            int: The count of available books.
+        """
+        return self.book_records.filter(status=BorrowBook.STATUS.BORROWED).count()
+    
+    
+    def reserved_books(self):
+        """
+        Returns the number of books not available for reservation.
+
+        Returns:
+            int: The count of non-available books.
+        """
+        return self.book_records.filter(status=BorrowBook.STATUS.RESERVED).count()
+    
       
+class BorrowBook(models.Model):
+    class STATUS:
+        BORROWED = "B"
+        RESERVED = "R"
+        RETURNED  = "RR"
+        
+        CHOICES = [
+            (BORROWED, "Borrowed"),
+            (RESERVED, "Reserved"),
+            (RETURNED, "Returned"),
+            
+        ]
+    library               = models.ForeignKey(Library, on_delete=models.CASCADE, related_name="book_records")
+    book                  = models.ForeignKey("Book", on_delete=models.CASCADE, related_name="book_records")
+    member                = models.ForeignKey("Member", on_delete=models.CASCADE, related_name="book_records")
+    status                = models.CharField(choices=STATUS.CHOICES, max_length=2)
+    due_date              = models.DateField()
+    return_date           = models.DateField(blank=True, null=True)
     
     
+    class Meta:
+        verbose_name = "Loan Book"
+        verbose_name_plural = "Loan Books"
+    
+    def __str__(self) -> str:
+        return f"The book <{self.book.title.title()}> is loaned to <{self.member.full_name}> from <{self.library.name.title()}>"
+    
+    def can_borrow(self):
+        pass
+        # current_borrowed_count = self.num_of_books_borrowed  
+        # return current_borrowed_count < self.library.max_borrow_limit
+    
+    def is_overdue(self):
+        current_date = timezone.now().date()
+        return self.due_date < current_date and self.return_date == None
+    
+    @classmethod
+    def overdue_books(cls, library):
+        """Return the number of overdue books for a given library."""
+        current_date = timezone.now().date()
+        return cls.objects.filter(library=library, status=cls.STATUS.BORROWED, due_date__lt=current_date, return_date__isnull=True).count()
+    
+    
+
+  
+
 class Member(models.Model):
-    first_name      = models.CharField(max_length=40)
-    last_name       = models.CharField(max_length=40)
-    email           = models.EmailField(max_length=40, unique=True)
-    membership_date = models.DateTimeField(auto_now_add=True)
-    library         = models.ManyToManyField(Library, related_name="members")
-    borrow_records  = models.ManyToManyField("BorrowRecord", related_name="members", blank=True)
-    
+    first_name            = models.CharField(max_length=40)
+    last_name             = models.CharField(max_length=40)
+    email                 = models.EmailField(max_length=40, unique=True)
+    membership_date       = models.DateTimeField(auto_now_add=True)
+    library               = models.ManyToManyField(Library, related_name="members")
+    limit_reached         = models.BooleanField(default=False)
+
+    @property
+    def full_name(self):
+        """Returns the full name of the member"""
+        return f"{self.first_name.title()} {self.last_name.title()}"
     
     @property
     def total_num_of_books_borrowed(self):
         """The total number of books borrowed"""
-        return self.borrow_records.count()
+        return self.book_records.count()
     
     @property
     def total_libraries(self):
         """Returns the total number of libraries the member is associated with."""
         return self.library.count()
 
+    def __str__(self) -> str:
+        return f"{self.first_name.title()} {self.last_name.title()}"
     
-
-class BorrowRecord(models.Model):
-    library               = models.ForeignKey(Library, on_delete=models.CASCADE, related_name="member")
-    due_date              = models.DateField()
-    return_date           = models.DateField()
-    num_of_books_borrowed = models.PositiveSmallIntegerField()
-    limit_reached         = models.BooleanField(default=False)
-
-    def can_borrow(self):
-        current_borrowed_count = self.num_of_books_borrowed  
-        return current_borrowed_count < self.library.max_borrow_limit
-
-           
            
 class Author(models.Model):
     first_name   = models.CharField(max_length=40)
@@ -67,6 +139,12 @@ class Author(models.Model):
         """The full name of the user"""
         return f"{self.first_name.title()} {self.last_name.title()}"
     
+    def count_written_books(self):
+        """The number of books the author has created"""
+        return self.books.count()
+    
+
+
 class Book(models.Model):
     class Genre:
         ACTION = "A"
@@ -100,16 +178,24 @@ class Book(models.Model):
         ]
 
     title            = models.CharField(max_length=100)
-    ISBN             = models.CharField(max_length=10)
+    ISBN             = models.CharField(max_length=10, unique=True)
     publication_date = models.DateField()
     genre            = models.CharField(choices=Genre.CHOICES, default=Genre.ACTION, max_length=3)
     author           = models.ManyToManyField(Author, related_name="books")
     created_on       = models.DateTimeField(auto_now_add=True)
     modified_on      = models.DateTimeField(auto_now=True)
-    is_book_avaiable = models.BooleanField(default=True)
-    library          = models.ForeignKey(Library, on_delete=models.CASCADE, related_name="book")
+    is_book_available = models.BooleanField(default=True)
+    library          = models.ForeignKey(Library, on_delete=models.CASCADE, related_name="books")
 
     @property
     def num_of_authors(self):
         """The number of authors for a given book"""
         return self.author.count()
+    
+    @property
+    def library_location(self):
+        """The location where the book can be found"""
+        return self.library.name
+
+    def __str__(self) -> str:
+        return self.title.title()
